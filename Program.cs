76f1894433cf7bcce5eab9acae5f1c95825a505e
Alessandra Lucas <alessandra1702@hotmail.com>
2024-05-14 20:42:39 -0300

    SQL Injection

--- Program.cs
+++ Program.cs
@@ -12,13 +12,22 @@ namespace BaltaDataAccess
             const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True";
         
             var category = new Category();
-            var insertSql = INSERT INTO [Category] VALUES(NEWID(), id, title, url, summary, order, description, featured);
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Description = "Categoria destinada a serviços do AWS";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Feature = false;
+
+            var insertSql = @"INSERT INTO 
+                                [Category] 
+                             VALUES(NEWID(), id, title, url, summary, order, description, featured)";
 
             using (var connection = new SqlConnection(connectionString))
             {  
                 var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category] ORDER BY [Title]");
-
-                foreach(var category in categories)
+                foreach(var item in categories)
                 {
                     Console.WriteLine($"{category.Id} - {category.Title}");
                 }
