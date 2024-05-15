a0da85c32d32535d4189e4fcd5f8aeaa69e35243
Alessandra Lucas <alessandra1702@hotmail.com>
2024-05-15 19:37:01 -0300

    Dapper - Update

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using BaltaDataAccess.Models;
 using Dapper;
 using Microsoft.Data.SqlClient;
@@ -10,7 +10,26 @@ namespace BaltaDataAccess
         static void Main(string[] args)
         {
             const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$;TrustServerCertificate=True";
-        
+           
+            using (var connection = new SqlConnection(connectionString))
+            {  
+                UpdateCategory(connection);
+                ListCategories(connection);
+                //CreateCategory(connection);
+            }  
+        }
+
+        static void ListCategories(SqlConnection connection)
+        {
+            var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category] ORDER BY [Title]");
+            foreach(var item in categories)
+            {
+                Console.WriteLine($"{item.Id} - {item.Title}");
+            }
+
+        }
+        static void CreateCategory(SqlConnection connection)
+        {
             var category = new Category();
             category.Id = Guid.NewGuid();
             category.Title = "Amazon AWS";
@@ -33,9 +52,7 @@ namespace BaltaDataAccess
                                 @Description, 
                                 @Featured)";
 
-            using (var connection = new SqlConnection(connectionString))
-            {  
-                // vai executar o Insert e mapear os parametros
+            // vai executar o Insert e mapear os parametros
                 var rows = connection.Execute(insertSql, new
                 {
                     category.Id,
@@ -49,15 +66,21 @@ namespace BaltaDataAccess
 
                 Console.WriteLine($"{rows} linhas inseridas.");
 
-                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category] ORDER BY [Title]");
-                foreach(var item in categories)
-                {
-                    Console.WriteLine($"{category.Id} - {category.Title}");
-                }
-            }   
-          
         }
-    }
-}
 
+        static void UpdateCategory(SqlConnection connection)
+        {
+            var updateQuery = "UPDATE [Category] SET [Title]=@title WHERE [Id]=@id";
+            var rows = connection.Execute(updateQuery, new
+            {
+                //  struct Guid representa um identificador único global
+                id = new Guid("af3407aa-11ae-4621-a2ef-2028b85507c4"),
+                title = "Frontend 2021"
+            });
+
+            Console.WriteLine($"{rows} registros atualizados");
+
+        }
+    }    
+}
 
