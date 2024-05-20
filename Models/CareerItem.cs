944e57a6874b7fe44df6de7cda5b0cfaab94a130
Alessandra Lucas <alessandra1702@hotmail.com>
2024-05-20 20:37:58 -0300

    correção no GUID

--- Models/CareerItem.cs
+++ Models/CareerItem.cs
@@ -2,11 +2,11 @@ namespace BaltaDataAccess.Models
 {
     public class CareerItem
     {
-        public int Guid Id { get; set; }
+        public Guid Id { get; set; }
 
         public string Title { get; set; }
 
         // objeto do tipo complexo
-        public Course Course{ get; set; }
+        public Course Course { get; set; }
     }
 }
--- Models/Course.cs
+++ Models/Course.cs
@@ -2,7 +2,7 @@ namespace BaltaDataAccess.Models
 {
     public class Course
     {
-        public int Guid Id{ get; set; }
+        public Guid Id { get; set; }
 
         public string Title { get; set; }
     }
